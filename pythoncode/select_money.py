c41aad88c22d23d72d9407717118f655d3255f23
mdd1122 <836926001@qq.com>
2021-06-07 21:50:26 +0800

    提交课后作业：发工资

---
+++ pythoncode/select_money.py
@@ -0,0 +1,3 @@
+import money
+def select_money():
+    print('当前存款是：' , money.save_money)
---
+++ pythoncode/send_money.py
@@ -0,0 +1,4 @@
+import money
+def send_money():
+    money.save_money=money.save_money+1000
+    print('发工资啦')
